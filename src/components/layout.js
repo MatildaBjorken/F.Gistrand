731ddadc381699f8fa315a2604171999aa861f81
MatildaBjorken <matilda.bjork@hyperisland.se>
2021-08-11 16:55:13 +0200

    landing page

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,34 +1,28 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React, { useState } from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
-const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  const isRootPath = location.pathname === rootPath
-  let header
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query bioQueryAndBioQuery {
+      site {
+        siteMetadata {
+          social {
+            instagram
+          }
+        }
+      }
+    }
+  `)
 
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
+  const social = data.site.siteMetadata?.social
 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
+    <div className="global-wrapper">
+      <nav></nav>
+
       <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
-      </footer>
+
+      <footer></footer>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,89 +1,218 @@
-import * as React from "react"
-import { Link, graphql } from "gatsby"
-
-import Bio from "../components/bio"
+import React, { useState, useRef, useEffect } from "react"
+import { gsap, TweenLite, Power3 } from "gsap/all"
+//Components
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata?.title || `Title`
-  const posts = data.allMarkdownRemark.nodes
+import { ScrollTrigger } from "gsap/ScrollTrigger"
 
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <Seo title="All posts" />
-        <Bio />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
-    )
+const Index = () => {
+  gsap.registerPlugin(TweenLite, Power3, ScrollTrigger)
+  const testimonials = [
+    {
+      name: "Julia Cameron",
+      title: "001",
+      image: `${require("./images/filippa.jpg").default}`,
+      quote:
+        "It's all good. I was amazed at the quality of the Design. We've seen amazing results already.",
+    },
+    {
+      name: "Mark Jacobs",
+      title: "002",
+      image: `${require("./images/filippa.jpg").default}`,
+      quote:
+        "The rebranding has really helped our business. Definitely worth the investment.",
+    },
+    {
+      name: "Lisa Bearings",
+      title: "003",
+      image: `${require("./images/filippa.jpg").default}`,
+      quote:
+        "The service was excellent. Absolutely wonderful! A complete redesign did it for us.",
+    },
+  ]
+
+  let imageList = useRef(null)
+  let testimonialList = useRef(null)
+  const imageWidth = 340
+
+  const [state, setState] = useState({
+    isActive1: true,
+    isActive2: false,
+    isActive3: false,
+  })
+
+  useEffect(() => {
+    TweenLite.to(testimonialList.children[0], 0, {
+      opacity: 1,
+    })
+  }, [])
+
+  //Image transition
+  const slideLeft = (index, duration, multiplied = 1) => {
+    TweenLite.to(imageList.children[index], duration, {
+      x: -imageWidth * multiplied,
+      ease: Power3.easeOut,
+    })
+  }
+
+  const slideRight = (index, duration, multiplied = 1) => {
+    TweenLite.to(imageList.children[index], duration, {
+      x: imageWidth * multiplied,
+      ease: Power3.easeOut,
+    })
+  }
+
+  const scale = (index, duration) => {
+    TweenLite.from(imageList.children[index], duration, {
+      scale: 1.2,
+      ease: Power3.easeOut,
+    })
+  }
+
+  //Content transition
+
+  const fadeOut = (index, duration) => {
+    TweenLite.to(testimonialList.children[index], duration, {
+      opacity: 0,
+    })
+  }
+
+  const fadeIn = (index, duration) => {
+    TweenLite.to(testimonialList.children[index], duration, {
+      opacity: 1,
+      delay: 1,
+    })
+  }
+
+  const nextSlide = () => {
+    if (imageList.children[0].classList.contains("active")) {
+      setState({ isActive1: false, isActive2: true })
+      //Image transition
+      slideLeft(0, 1)
+      slideLeft(1, 1)
+      scale(1, 1)
+      slideLeft(2, 1)
+      slideLeft(2, 0)
+      fadeOut(0, 1)
+      fadeIn(1, 1)
+    } else if (imageList.children[1].classList.contains("active")) {
+      setState({ isActive2: false, isActive3: true })
+      //Image transition
+      slideRight(0, 1)
+      slideLeft(1, 1, 2)
+      slideLeft(2, 1, 2)
+      scale(2, 1)
+      //content transition
+      fadeOut(1, 1)
+      fadeIn(2, 1)
+    } else if (imageList.children[2].classList.contains("active")) {
+      setState({ isActive1: true, isActive3: false })
+      //Image transition
+      slideLeft(2, 1, 3)
+      slideLeft(0, 1, 0)
+      slideLeft(1, 0, 0)
+      scale(0, 1)
+      //content transition
+      fadeOut(2, 1)
+      fadeIn(0, 1)
+    }
+  }
+
+  const prevSlide = () => {
+    if (imageList.children[0].classList.contains("active")) {
+      setState({ isActive1: false, isActive3: true })
+      //Image transition
+      slideLeft(2, 0, 3)
+      slideLeft(2, 1, 2)
+      scale(2, 1)
+      slideRight(0, 1)
+      slideRight(1, 1)
+      //content transtion
+      fadeOut(0, 1)
+      fadeIn(2, 1)
+    } else if (imageList.children[1].classList.contains("active")) {
+      setState({ isActive2: false, isActive1: true })
+      //Image transition
+      slideLeft(0, 0)
+      slideRight(0, 1, 0)
+      slideRight(1, 1, 0)
+      slideRight(2, 1, 2)
+      scale(0, 1)
+      //content transtion
+      fadeOut(1, 1)
+      fadeIn(0, 1)
+    } else if (imageList.children[2].classList.contains("active")) {
+      setState({ isActive2: true, isActive3: false })
+      slideLeft(2, 1)
+      slideLeft(1, 0, 2)
+      slideLeft(1, 1)
+      scale(1, 1)
+      //content transtion
+      fadeOut(2, 1)
+      fadeIn(1, 1)
+    }
   }
 
   return (
-    <Layout location={location} title={siteTitle}>
-      <Seo title="All posts" />
-      <Bio />
-      <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
+    <Layout>
+      <Seo title="Home" />
+      <div className="header">
+        <h1 className="header-1">Filippa</h1>
+        <h1 className="header-2">Gistrand</h1>
+      </div>
 
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
-          )
-        })}
-      </ol>
+      <div className="testimonial-section">
+        <div className="testimonial-container">
+          <div onClick={nextSlide} className="img-nav">
+            <p className="prev">next</p>
+          </div>
+          <div className="sidebar-text">Refine your image.</div>
+          <div className="inner">
+            <div className="t-image">
+              <ul ref={el => (imageList = el)}>
+                <li className={state.isActive1 ? "active" : ""}>
+                  <img src={testimonials[0].image} alt={testimonials[0].name} />
+                </li>
+                <li className={state.isActive2 ? "active" : ""}>
+                  <img src={testimonials[1].image} alt={testimonials[0].name} />
+                </li>
+                <li className={state.isActive3 ? "active" : ""}>
+                  <img src={testimonials[2].image} alt={testimonials[0].name} />
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="img-nav" onClick={prevSlide}>
+            <p className="next">prev</p>
+          </div>
+          <div className="t-content">
+            <ul ref={el => (testimonialList = el)} className="test">
+              <li className={state.isActive1 ? "active" : ""}>
+                <div className="content-inner">
+                  <h3 className="name">{testimonials[0].name}</h3>
+                  <h4 className="title">{testimonials[0].title}</h4>
+                </div>
+              </li>
+              <li className={state.isActive2 ? "active" : ""}>
+                <div className="content-inner">
+                  <h3 className="name">{testimonials[1].name}</h3>
+                  <h4 className="title">{testimonials[1].title}</h4>
+                </div>
+              </li>
+              <li className={state.isActive3 ? "active" : ""}>
+                <div className="content-inner">
+                  <h3 className="name">{testimonials[2].name}</h3>
+                  <h4 className="title">{testimonials[2].title}</h4>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
 
-export default BlogIndex
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      nodes {
-        excerpt
-        fields {
-          slug
-        }
-        frontmatter {
-          date(formatString: "MMMM DD, YYYY")
-          title
-          description
-        }
-      }
-    }
-  }
-`
+export default Index
